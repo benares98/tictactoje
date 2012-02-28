8c1f0f19a8b52b5a0cdfd057e4c6f80e96b28dd2
benares_98 <benares_98@yahoo.com>
2012-02-28 12:35:06 -0600

    Refactored a few of the functions to make it more readable (WTF does comp-win your-win really mean ;))  Struggling abit on how to cycle off player/computer moves.

--- src/tictactoje/core.clj
+++ src/tictactoje/core.clj
@@ -1,9 +1,9 @@
 (ns tictactoje.core
-  (:use (clojure set)))
+  (:use [clojure.set :only [intersection]]))
 
 (def winlist [0 0 0 0 0 0 0 0])
-(def user-win (ref winlist))
-(def comp-win (ref winlist))
+(def x (ref winlist))
+(def o (ref winlist))
 
 (def initial-board [\_ \_ \_
                     \_ \_ \_
@@ -11,55 +11,60 @@
 
 (def board (ref initial-board))
 
-(defn reset[] (dosync (ref-set user-win winlist)
-                      (ref-set comp-win winlist)
+(defn reset[] (dosync (ref-set x winlist)
+                      (ref-set o winlist)
                       (ref-set board initial-board)))
-(defn print-board [board] ())
 
 (def win-positions [#{0 1 2}
                     #{3 4 5}
                     #{6 7 8}, #{0 3 6} #{1 4 7} #{2 5 8}, #{0 4 8} #{2 4 6}])
 
-(def position-win [#{0 1 3}  #{0 4}   #{0 5 7}
-                   #{1 3}  #{1 4 6 7} #{1 5}
-                   #{2 3 7}  #{2 4}   #{2 5 6}])
+(def weighted-positions [#{0 1 3}  #{0 4}   #{0 5 7}
+                         #{1 3}  #{1 4 6 7} #{1 5}
+                         #{2 3 7}  #{2 4}   #{2 5 6}])
 
-(defn update-wins [winlist pos]
+(defn update-wins [player pos]
   (let [inc-found (fn [pos coll val] (if (contains? coll pos) (inc val) val))]
-    (dosync (ref-set winlist (map #(inc-found pos %1 %2) win-positions @winlist)))))
+    (dosync (ref-set player (map #(inc-found pos %1 %2) win-positions @player)))))
 
 (defn update-board [board pos player]
   (dosync (ref-set board (assoc @board pos player))))
-                             
+
 (defn winning-positions [winlist rank]
   (let[ranked-positions (fn[rank positions ranking] (if (= rank ranking)positions))]
     (mapcat #(ranked-positions rank %1 %2) win-positions @winlist)))
 
 (defn available-positions [coll]
-  (let [index (fn [coll] map vector (iterate inc 1) coll)]
+  (let [index (fn [coll] (map vector (iterate inc 0) coll))]
     (for [[i v] (index coll) :when (= \_ v)] i)))
 
-(defn best-play
+(defn max-weight
   ([pos]
      pos)
   ([pos1 pos2]
-     (if (< (count (nth position-win pos1)) (count (nth position-win pos2)))
+     (if (< (count (nth weighted-positions pos1)) (count (nth weighted-positions pos2)))
        pos2
        pos1))
   ([pos1 pos2 & more]
-     (reduce best-play (best-play pos1 pos2) more)))
+     (reduce max-weight (max-weight pos1 pos2) more)))
 
-(defn positional-play [winlist rank]
-  (let [positions (intersection (available-positions board) (winning-positions winlist rank))]
-    (best-play positions)))
+(defn positional-play [player rank]
+  (let [positions (intersection (set (available-positions @board)) (set (winning-positions player rank)))]
+    (apply max-weight positions)))
 
-(defn comp-play []
-  (let [enemy-rank (apply max @user-win) comp-rank (apply max @comp-win)]
-    (if (> comp-rank enemy-rank)
-      (positional-play comp-win comp-rank)
-      (positional-play user-win enemy-rank))))
+(defn suggested-position [player enemy]
+  (let [rank (apply max @player) enemy-rank (apply max @enemy)]
+    (if (>= rank enemy-rank)
+      (positional-play player rank)
+      (positional-play enemy enemy-rank))))
 
-(defn play [pos]
-  (do (update-wins user-win pos)
-      (update-wins comp-win comp-play)))
+(defn progress [player pos]
+  (do (update-wins player pos)
+      (update-board board pos (name `~player))))
 
+(defn play
+  ([player pos] (progress player pos)))
+
+(defn auto-play [player enemy]
+  (let[pos (suggested-position (player enemy))]
+    (progress player pos)))
